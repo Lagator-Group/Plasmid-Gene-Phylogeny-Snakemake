4a1ed75aeaa602ef3094ee8b66430056d3effe96
acidsh0t <91309688+acidsh0t@users.noreply.github.com>
2024-04-01 15:13:23 +0100

    codeium comments

--- bin/scripts/merge_AA_gff.py
+++ bin/scripts/merge_AA_gff.py
@@ -5,10 +5,6 @@ import shutil
 plasmid_df = pd.read_csv(snakemake.input[0])
 gene_of_interest = snakemake.wildcards.gene
 
-'''#debugging
-plasmid_df = pd.read_csv('plasmid_summary.csv')
-gene_of_interest = 'finO''''
-
 
 print(gene_of_interest)
 
@@ -34,6 +30,7 @@ for gene_list in plasmid_df['TF Gene Simplified']:
     # Increment the index of the row in plasmid_df
     n += 1
 
+# Create the output directories if they don't already exist
 try:
     os.mkdir('merged_AA_temp')
 except:
@@ -49,53 +46,69 @@ try:
 except:
     pass
 
+# For each plasmid in the list of plasmids to process,
+# retrieve the sprot_df for that plasmid and extract the gene names and locus tags
 for plasmid in plasmid_list:
-
+    print(f"Processing plasmid {plasmid}")
     sprot_df = pd.read_csv(f"sprot_TF/{plasmid}.tsv", sep="\t")
-    prokka = f'prokka/{plasmid}/{plasmid}.faa'
 
+    # Extract the list of gene names and locus tags for this plasmid
     locus_list = []
 
-    n=0
+    n = 0  # index of row in sprot_df
     for gene_list in sprot_df['Gene Names']:
         gene_list = str(gene_list)
         gene_list = gene_list.split(' ')
-
+        print(f"Gene list: {gene_list}")
         if gene_of_interest in gene_list:
-
+            print(f"{gene_of_interest} is in gene list for plasmid {plasmid}")
+            # If the gene of interest is in the list of genes for this plasmid,
+            # copy the GFF file to the output directory and extract the locus tag
             gff_source = f'prokka/{plasmid}/{plasmid}.gff'
             gff_destination = f'prokka_gff/{gene_of_interest}/{plasmid}.gff'
 
+            print(f"Copying {gff_source} to {gff_destination}")
             shutil.copy(gff_source, gff_destination)
 
-            print(prokka)
             locus = sprot_df['Locus Tag'][n]
             locus_list.append(locus)
+            print(f"Locus tag for {plasmid}: {locus}")
         else:
-            pass
-        n+=1    
+            pass  # Do nothing if the gene of interest is not in this plasmid's list
+
+        n += 1
+
+    # Read the FASTA file for this plasmid and extract the sequence for each locus tag
+    # that was found in the sprot_df
+    prokka = f'prokka/{plasmid}/{plasmid}.faa'
 
     with open(prokka, 'r') as f:
-        data=f.read()
-        
+        data = f.read()
+
         for locus in locus_list:
-            start=data.find(locus)
-            end=data.find('>', start+1)
+            start = data.find(locus)
+            end = data.find('>', start + 1)
             result = data[start:end]
             result = result.replace(' ', '_')
-            print(result)
 
+            # Write the extracted sequence to the output file, along with the plasmid name and locus tag
             with open(f'merged_AA_temp/{gene_of_interest}_temp.fasta', 'a') as out:
                 out.write(f'>{plasmid}_{result}\n')
                 out.close()
+                print(f"Wrote {plasmid}_{result} to output file")
 
 # Open the input and output files
 with open(f'merged_AA_temp/{gene_of_interest}_temp.fasta', 'r') as input_file, open(f'merged_AA_temp/{gene_of_interest}.fasta', 'w') as output_file:
+    print(f"Reading from {f'merged_AA_temp/{gene_of_interest}_temp.fasta'}...")
     # Read each line from the input file
     for line in input_file:
+        print(f"Read line: {line.strip()}")
         # Check if the line contains only '\n'
         if line.strip() != '':
             # Write non-empty lines to the output file
             output_file.write(line)
+            print(f"Wrote line to {f'merged_AA_temp/{gene_of_interest}.fasta'}")
 
+print(f"Deleting {f'merged_AA_temp/{gene_of_interest}_temp.fasta'}...")
 os.remove(f'merged_AA_temp/{gene_of_interest}_temp.fasta')
+
--- bin/scripts/remove_protein.py
+++ bin/scripts/remove_protein.py
@@ -1,19 +1,46 @@
+# Read in fasta file
 file_in = snakemake.input[0]
+
+# Write output to a new fasta file
 file_out = snakemake.output[0]
 
+print(f"Reading from {file_in}...")
 with open(file_in, 'r') as f_in, open(file_out, 'w') as f_out:
+    # Read in all lines from the input file
     lines = f_in.readlines()
+
+    print(f"Read {len(lines)} lines from input file.")
+
+    # Initialize a new list to store the modified lines
     new_lines = []
-    for line in lines:
+
+    # Iterate over each line in the file
+    for i, line in enumerate(lines):
+        print(f"Processing line {i+1}...")
+
+        # Check if the line starts with '>'
         if line.startswith('>'):
+            # Split the line on '_'
             line_new = line.split('_')
+
+            # If the first part of the split line is less than 8 characters long
             if len(line_new[0]) < 8:
-                new_lines.append(line_new[0] + '_' + line_new[1]+'\n')
-            else:   
-                new_lines.append(line_new[0]+'\n')
+                # Add the first and second parts of the split line back together,
+                # with an underscore in between, and a newline at the end
+                new_line = line_new[0] + '_' + line_new[1] + '\n'
+                print(f"Line {i+1}: Writing '{new_line}' to output file.")
+                new_lines.append(new_line)
+            # Otherwise, just add the first part of the split line, with a newline at the end
+            else:
+                new_line = line_new[0] + '\n'
+                print(f"Line {i+1}: Writing '{new_line}' to output file.")
+                new_lines.append(new_line)
+
+        # If the line doesn't start with '>', just add it to the new list as is
         else:
+            print(f"Line {i+1}: Writing '{line}' to output file.")
             new_lines.append(line)
+
+    # Write all the new lines to the output file
+    print(f"Writing {len(new_lines)} lines to output file.")
     f_out.writelines(new_lines)
-
-
-
